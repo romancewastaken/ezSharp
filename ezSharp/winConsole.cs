60148cb84193513aa6046a69cb8b5e4528cc4d41
romance <66000596+romance999@users.noreply.github.com>
2022-09-05 18:56:51 -0400

    Create a console

---
+++ ezSharp/winConsole.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ezSharp
+{
+    internal class winConsole
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+
+
+        public static extern bool AllocConsole();    //allocate console
+
+        //use this function when your application starts + when you want to update the console
+        public static void Update(string title, string message)
+        {
+            try
+            {
+                Console.Title = title;
+                Console.WriteLine(message);
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+        }
+    }
+}
