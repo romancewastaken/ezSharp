a26eb9b165efb2c77ec7d1e3dda1948b375136fe
romance <66000596+romance999@users.noreply.github.com>
2022-09-05 19:04:30 -0400

    Basic webclient functions

---
+++ ezSharp/net.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+
+namespace ezSharp
+{
+    internal class net
+    {
+        public static void downloadFile(string link, string path)
+        {
+            try
+            {
+                WebClient client = new WebClient();
+                client.DownloadFileAsync(new Uri(link), path);
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+        }
+
+        public string readString(string link)
+        {
+            try
+            {
+                WebClient client = new WebClient();
+                return client.DownloadString(link);
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
