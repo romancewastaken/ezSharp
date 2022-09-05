a8fdcc1b1f2e232c2b6cc597895a850e12161167
romance <66000596+romance999@users.noreply.github.com>
2022-09-05 18:57:28 -0400

    Create a toast window

---
+++ ezSharp/toastWindow.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+
+namespace ezSharp
+{
+    internal class toastWindow
+    {
+        public static void Create(string topMessage, string bottomMessage)
+        {
+            new ToastContentBuilder()
+            .AddArgument("action", "viewConversation")
+            .AddArgument("conversationId", 9813)
+            .AddText(topMessage)
+            .AddText(bottomMessage)
+            .Show();
+        }
+    }
+}
