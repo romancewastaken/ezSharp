179adb711ca0586b82539efa69bbc51f6d66c34d
romance <66000596+romancewastaken@users.noreply.github.com>
2022-11-23 02:43:10 -0500

    this shit is busted af

---
+++ ezSharp/genericTypes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ezSharp
+{
+    public class genericTypes
+    {
+        public static string[] stringArr = { "test", "test2" };
+        int[] intArr;
+        float[] floatArr;
+        double[] doubleArr;
+
+
+
+
+        /// <param name="array">Name of array</param>
+        public static void appendToArr<T>(T[] array)
+        {
+
+        }
+
+        /// <param name="array">Name of array</param>
+        public static List<T> returnArr<T>(T[] array)
+        {
+            List<T> temp = new List<T>();
+
+            foreach (T item in array)
+            {
+                temp.Add(item);
+            }
+
+            return temp;
+        }
+    }
+}
